41506909bacfc447dfc03264792e6c0a7284cd68
Kendall Adkins <adkins.kendall90@gmail.com>
2026-06-01 19:09:17 -0700

    feat: redirect / to /home, retire splash

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,5 @@
-import Splash from "./Splash";
-import { getAccountMmr } from "@/lib/db/queries";
-export default async function Page() {
-  const data = await getAccountMmr();
-  const a = data?.account ?? {
-    name: "ST1CCS",
-    tag: "STONE",
-    account_level: 0,
-    region: "na",
-  };
-  return (
-    <Splash
-      name={a.name}
-      tag={a.tag}
-      level={a.account_level}
-      region={a.region}
-    />
-  );
+import { redirect } from "next/navigation";
+
+export default function Page() {
+  redirect("/home");
 }
