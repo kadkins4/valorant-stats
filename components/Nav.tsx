ec929ff3ac4a1128523f1331beee0fe63cabd01b
Kendall Adkins <adkins.kendall90@gmail.com>
2026-06-04 10:53:16 -0700

    Drop no-op aria-disabled from Soon nav spans

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -25,7 +25,6 @@ export default async function Nav() {
         t.soon ? (
           <span
             key={t.href}
-            aria-disabled="true"
             style={{
               padding: "4px 14px",
               borderRadius: 6,
